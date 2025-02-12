346d3eeca5bb72e236243114a2a6c57cd024c35d
Rodrigo Calvo Mèrida <160269872+Willsongamf@users.noreply.github.com>
2025-02-12 16:07:27 +0100

    Commit_Parte3

---
+++ Paret3/Parte3/src/main/java/com/example/parte3/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.parte3;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {    @FXML
+    private Label welcomeText;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
